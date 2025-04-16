2a5666c7202ce85e5e26246cba287152b6a5f40b
Alfio <tomarchioalfio5@gmail.com>
2025-04-16 20:40:47 +0100

    move math.h from .h to .c

--- CLargs.c
+++ CLargs.c
@@ -11,6 +11,7 @@
 
 #include "CLargs.h"
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- CLargs.h
+++ CLargs.h
@@ -16,7 +16,6 @@
 extern "C" {
 #endif
 
-#include <math.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
